3653fdb331f85d0e17e1f04428071037b65dd021
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2025-09-30 12:30:56 +0300

    Swapping

--- src/contracts/txUtil.ts
+++ src/contracts/txUtil.ts
@@ -15,45 +15,66 @@ type Output = {
 }
 
 export class TxUtil extends SmartContractLib {
-    static readonly MAX_INPUTS_OUTPUTS_READ_OUTPUT: bigint = 32n;
+    static readonly MAX_INPUTS_OUTPUTS_READ_OUTPUT: bigint = 32n
 
     // get i-th outpoint's txid
     @method()
     static getPrevoutTxid(prevouts: ByteString, i: bigint): ByteString {
-        let offset: bigint = i * Constants.OutpointLen
+        const offset: bigint = i * Constants.OutpointLen
         return slice(prevouts, offset, offset + Constants.TxIdLen)
+    }
+
+    @method()
+    static verifyHolders(prevouts: ByteString, txid: ByteString): boolean {
+        // Validate inputs
+        assert(
+            TxUtil.getPrevoutTxid(prevouts, 1n) === txid,
+            'input 1 is not from correct transaction'
+        )
+        assert(
+            TxUtil.getPrevoutTxid(prevouts, 2n) === txid,
+            'input 2 is not from correct transaction'
+        )
+
+        return true
     }
 
     // Unlocks the token/coin holders for liquidity pools
     @method()
     static unlockHolder(
-        prevouts: ByteString, 
-        outputIndex: bigint, 
-        pos: bigint, 
+        prevouts: ByteString,
+        outputIndex: bigint,
+        pos: bigint,
         txid: ByteString
     ): boolean {
         // Validate that the input is the same position as the output
-        assert(outputIndex === pos, 'incorrect output position');
+        assert(outputIndex === pos, 'incorrect output position')
 
         // Validate input 0
-        const inputTxid: ByteString = txid;
-        assert(TxUtil.getPrevoutTxid(prevouts, 0n) === inputTxid, 'input 0 is not from correct transaction');
+        const inputTxid: ByteString = txid
+        assert(
+            TxUtil.getPrevoutTxid(prevouts, 0n) === inputTxid,
+            'input 0 is not from correct transaction'
+        )
 
         // Validate other output
-        let assertPos: bigint = 1n;
+        let assertPos: bigint = 1n
         if (pos === 1n) {
-            assertPos = 2n;
+            assertPos = 2n
         }
 
-        assert(TxUtil.getPrevoutTxid(prevouts, assertPos) === inputTxid, 'other input is not from correct transaction');
+        assert(
+            TxUtil.getPrevoutTxid(prevouts, assertPos) === inputTxid,
+            'other input is not from correct transaction'
+        )
 
-        return true;
+        return true
     }
 
     // get i-th outpoint's output index
     @method()
     static getPrevoutOutputIdx(prevouts: ByteString, i: bigint): bigint {
-        let offset = i * Constants.OutpointLen
+        const offset = i * Constants.OutpointLen
         return Utils.fromLEUnsigned(
             slice(
                 prevouts,
@@ -68,7 +89,7 @@ export class TxUtil extends SmartContractLib {
         // first 4 bytes version
         // 1 byte input num
         let pos: bigint = 4n
-        let ninputs: bigint = Utils.fromLEUnsigned(slice(tx, pos, pos + 1n))
+        const ninputs: bigint = Utils.fromLEUnsigned(slice(tx, pos, pos + 1n))
         pos = pos + 1n
         let script: ByteString = toByteString('')
         let satoshis: bigint = 0n
@@ -80,24 +101,24 @@ export class TxUtil extends SmartContractLib {
                 pos = pos + 36n
                 // 1 byte var
                 // script code + 4 bytes sequence
-                let varLen: bigint = Utils.fromLEUnsigned(
+                const varLen: bigint = Utils.fromLEUnsigned(
                     slice(tx, pos, pos + 1n)
                 )
                 if (varLen < 253) {
-                    let scriptLen: bigint = varLen
+                    const scriptLen: bigint = varLen
                     pos = pos + 1n + scriptLen + 4n
                 } else if (varLen == 253n) {
-                    let scriptLen: bigint = Utils.fromLEUnsigned(
+                    const scriptLen: bigint = Utils.fromLEUnsigned(
                         slice(tx, pos + 1n, pos + 3n)
                     )
                     pos = pos + 3n + scriptLen + 4n
                 } else if (varLen == 254n) {
-                    let scriptLen: bigint = Utils.fromLEUnsigned(
+                    const scriptLen: bigint = Utils.fromLEUnsigned(
                         slice(tx, pos + 1n, pos + 5n)
                     )
                     pos = pos + 5n + scriptLen + 4n
                 } else {
-                    let scriptLen: bigint = Utils.fromLEUnsigned(
+                    const scriptLen: bigint = Utils.fromLEUnsigned(
                         slice(tx, pos + 1n, pos + 9n)
                     )
                     pos = pos + 9n + scriptLen + 4n
@@ -105,18 +126,18 @@ export class TxUtil extends SmartContractLib {
             }
         }
 
-        let noutputs: bigint = Utils.fromLEUnsigned(slice(tx, pos, pos + 1n))
+        const noutputs: bigint = Utils.fromLEUnsigned(slice(tx, pos, pos + 1n))
         pos = pos + 1n
         assert(noutputs <= TxUtil.MAX_INPUTS_OUTPUTS_READ_OUTPUT)
         for (let i = 0n; i < TxUtil.MAX_INPUTS_OUTPUTS_READ_OUTPUT; i++) {
             if (i < noutputs) {
                 // 8 bytes value
-                let sats: bigint = Utils.fromLEUnsigned(
+                const sats: bigint = Utils.fromLEUnsigned(
                     slice(tx, pos, pos + 8n)
                 )
                 pos = pos + 8n
                 // script code
-                let varLen: bigint = Utils.fromLEUnsigned(
+                const varLen: bigint = Utils.fromLEUnsigned(
                     slice(tx, pos, pos + 1n)
                 )
                 let scriptLen: bigint = 0n
